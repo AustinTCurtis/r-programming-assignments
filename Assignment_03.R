9cacaa4e7963f0b519dd1d0c3265cf75bf528a88
AustinTCurtis <austincurtis@usf.edu>
2025-09-14 12:07:54 -0400

    add assignment_03

---
+++ Assignment_03.R
@@ -0,0 +1,24 @@
+# Define vectors
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_poll <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_poll <- c(12, 75, 43, 19, 1, 21, 19)
+
+# Combine into a data frame
+df_polls <- data.frame(Name, ABC_poll, CBS_poll)
+
+# Inspect structure and first rows
+str(df_polls)
+head(df_polls)
+
+# Summary statistics
+mean(df_polls$ABC_poll)
+median(df_polls$CBS_poll)
+range(df_polls[, c("ABC_poll", "CBS_poll")])
+
+# Add a new column for differences
+df_polls$Diff <- df_polls$CBS_poll - df_polls$ABC_poll
+
+# Quick plot
+barplot(df_polls$Diff, names.arg = df_polls$Name,
+        col = "steelblue", main = "Difference: CBS - ABC Polls",
+        ylab = "Difference in Poll %")
