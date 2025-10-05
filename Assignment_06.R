c696abc415073414a9ca16c6cd6b4304f1341eb7
AustinTCurtis <austincurtis@usf.edu>
2025-10-05 14:40:20 -0400

    add assignment_06

---
+++ Assignment_06.R
@@ -0,0 +1,15 @@
+# Define two 2x2 matrices A and B
+A <- matrix(c(2, 0, 1, 3), ncol = 2)
+B <- matrix(c(5, 2, 4, -1), ncol = 2)
+
+# Display A and B
+A
+B
+
+# Matrix addition
+A_plus_B <- A + B
+A_plus_B
+
+# Matrix subtraction
+A_minus_B <- A - B
+A_minus_B
